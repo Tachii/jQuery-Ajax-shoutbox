8431f8fcec3b2f84c1cd8cbcf4a77b5dfd20f81a
Tachi <zaveruha007@gmail.com>
2015-06-09 18:39:40 +0300

    jquery & ajax

--- js/script.js
+++ js/script.js
@@ -1,64 +1,23 @@
-*{
-	margin:0;
-	padding:0;
-}
-
-body{
-	font-family:"Arial", sans-serif;
-	background:#000;
-	color:#fff;
-	font-size:14px;
-}
-
-#container{
-	width:700px;
-	margin:40px auto;
-	overflow:auto;
-	border:#ccc 1px solid;
-}
-
-header{
-	background:#333;
-	padding:10px;
-	text-align:center;
-}
-
-#shouts{
-	height:200px;
-	border:#333 1px solid;
-	margin:30px;
-	padding:15px;
-}
-
-#shouts li{
-	list-style:none;
-	line-height:1.7em;
-}
-
-footer{
-	padding:10px;
-	background:#333;
-	text-align:center;
-}
-
-input#name,input#shout{
-	padding:3px;
-	height:15px;
-}
-
-input#name{
-	width:130px;
-	margin-right:20px;
-}
-
-input#shout{
-	width:270px;
-}
-
-input#submit{
-	padding:5px 10px;
-	border:0;
-	background:#666;
-	color:#fff;
-	cursor:pointer;
-}
+/**
+ * @author tachi
+ */
+$(document).ready(function(){
+	$('#submit').on('click', function(){
+		var name = $('#name').val();
+		var message = $('#shout').val();
+		//var date = getDate();
+		//var dataString = 'name = '+ name +'&shout = '+ shout +'&date=' + date;
+		
+		//Validation
+		if(name == '' || shout == ''){
+			alert('Please enter in your name and message');
+		} else {
+			$.ajax({
+				
+			});
+		}
+		
+		//Prevent Form from submitting
+		return false;
+	});
+});
