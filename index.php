2d2b8a8a0f5969ceb3a7b09821dcf43b67a5d29c
Tachi <zaveruha007@gmail.com>
2015-06-09 17:18:31 +0300

    PDO

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>jQuery ShoutBox</title>
+		<meta name="description" content="">
+		<meta name="author" content="tachi">
+		
+		<link rel="stylesheet" href="css/style.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="js/script.js"></script>
+		
+		
+	</head>
+
+	<body>
+		<div id="container">
+			<header>
+				<h1>ShoutBox</h1>
+			</header>
+			<div id="shouts">
+				<ul>
+					<li></li>
+				</ul>
+			</div>
+			<footer>
+				<form>
+					<label>Name:</label>
+					<input type="text" id="name" />
+					<label>Shout Text:</label>
+					<input type="text" id="shout" />
+					<input type="submit" id="submit" value="SHOUT!" />
+				</form>
+			</footer>
+		</div>
+	</body>
+</html>
