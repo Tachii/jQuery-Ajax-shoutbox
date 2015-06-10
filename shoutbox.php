cdf2e91e4b1cb94f5b858638205e7f2a1392459c
Tachi <zaveruha007@gmail.com>
2015-06-10 16:25:50 +0300

    Data manipulation

--- shoutbox.php
+++ shoutbox.php
@@ -1,18 +1,23 @@
 <?php 
-error_reporting(E_ALL);
-
+include 'database.php';
 
 if(isset($_POST['name']) && isset($_POST['message'])){
+    $data = array( 
+        'name' => $_POST['name'], 
+        'shout' => $_POST['message'], 
+        'date' => $_POST['date']
+    );
+
     try {
         // NOTICE: prepare() used and VALUES instead of VALUE
-        $STH = $DBH->prepare("INSERT INTO shouts (name, message, date) values (:name, :message, :date)");
+        $sth = $dbh->prepare("INSERT INTO shouts (name, message, date) values (:name, :message, :date)");
 
-		$STH->bindParam(':name', $_POST['name']);
-	    $STH->bindParam(':message', $_POST['message']);
-	    $STH->bindParam(':date', $_POST['date']);
-		
-		// NOTICE: $data has allready been supplied by bindParam()
-        $STH->execute();
+        $sth->bindParam(':name', $_POST['name']);
+        $sth->bindParam(':message', $_POST['message']);
+        $sth->bindParam(':date', $_POST['date']);
+
+        // NOTICE: $data has allready been supplied by bindParam()
+        $sth->execute();
     }
     catch(PDOException $e) {
         echo $e->getMessage();
